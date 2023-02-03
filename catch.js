e6ed40f9e88c33ddd6f8b0c0794a6c6250685fe7
yitong86 <sophiajiang86@hotmail.com>
2023-02-02 21:29:30 -0500

    tryCatch

---
+++ catch.js
@@ -0,0 +1,11 @@
+function reverseString(s) {
+    try {
+           s = s.split("").reverse().join("");  
+       }
+       catch(err) {
+               console.log(err.message); 
+       }
+       finally{
+           console.log(s);
+       }
+}
