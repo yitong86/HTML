a5f1d4bd5305b116aa4284cb9a82c7f08d30ec0c
yitong86 <sophiajiang86@hotmail.com>
2023-02-02 21:33:39 -0500

    array

--- catch.js
+++ catch.js
@@ -34,3 +34,17 @@ function getLetter(s) {
     }
     return letter;
 }
+function getSecondLargest(nums) {
+    let largest = 0
+     let secondLargest = 0
+     for(let i=0; i < nums.length; i++){
+         if(nums[i] > largest) {
+             secondLargest = largest
+             largest = nums[i]
+         }
+         else if (nums[i] != largest && nums[i]>secondLargest) {
+             secondLargest = nums[i]
+         }
+     }
+     return secondLargest
+ }
