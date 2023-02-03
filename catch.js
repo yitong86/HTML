6ec7fbdc2c2932a8009d3a7590e8dc15ddac6e21
yitong86 <sophiajiang86@hotmail.com>
2023-02-02 21:31:57 -0500

    switch

--- catch.js
+++ catch.js
@@ -9,3 +9,28 @@ function reverseString(s) {
            console.log(s);
        }
 }
+function isPositive(a) {
+    if (a == 0){
+        throw new Error('Zero Error');
+    }
+    else if (a < 0){
+        throw new Error('Negative Error');
+    }
+    else {
+        return 'YES'
+    }
+}
+function getLetter(s) {
+    let letter;
+    switch(true){
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';break;
+        case 'bcdfg'.includes(s[0]): 
+            letter = 'B';break;
+        case 'hjklm'.includes(s[0]): 
+            letter = 'C';break;
+        default: 
+            letter = 'D';break;
+    }
+    return letter;
+}
